619c892c153249b714d04fbd8442ff9cb7108e4c
yamacho <yamacho@users.noreply.github.com>
2020-03-10 17:10:32 +0900

    effectiveDamage 関数のテストコードを追加

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const assert = require('assert');
+const dc = require('../');
+
+describe('#effectiveDamage()', () => {
+
+  it('正常なダメージ計算ができる', () => {
+    assert.equal(dc.effectiveDamage(100, 50, 30), 83);
+  });
+})
