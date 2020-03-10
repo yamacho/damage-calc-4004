ab6cd8987469399afefac07b755b58d383adcb8f
yamacho <yamacho@users.noreply.github.com>
2020-03-10 17:33:07 +0900

    2000より大きい異常値に対するテストコードを追加

--- test/test.js
+++ test/test.js
@@ -14,4 +14,10 @@ describe('#effectiveDamage()', () => {
     assert.equal(dc.effectiveDamage(0, -1, 0), 0);
     assert.equal(dc.effectiveDamage(0, 0, -1), 0);
   });
+
+  it('2000より大きい異常値におけるダメージ計算ができる', () => {
+    assert.equal(dc.effectiveDamage(2001, 0, 0), 2000);
+    assert.equal(dc.effectiveDamage(300, 2150, 0), 14);
+    assert.equal(dc.effectiveDamage(300, 2000, 2001), 300);
+  });
 })
