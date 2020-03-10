048e4ae5bc1c6b929280693b67b7f3b370ca41a2
yamacho <yamacho@users.noreply.github.com>
2020-03-10 17:20:33 +0900

    負の異常値に対するテストコードを追加

--- test/test.js
+++ test/test.js
@@ -8,4 +8,10 @@ describe('#effectiveDamage()', () => {
   it('正常なダメージ計算ができる', () => {
     assert.equal(dc.effectiveDamage(100, 50, 30), 83);
   });
+
+  it('負の異常値におけるダメージ計算ができる', () => {
+    assert.equal(dc.effectiveDamage(-1, 0, 0), 0);
+    assert.equal(dc.effectiveDamage(0, -1, 0), 0);
+    assert.equal(dc.effectiveDamage(0, 0, -1), 0);
+  });
 })
